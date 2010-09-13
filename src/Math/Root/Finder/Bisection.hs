d2c1a26efe87a8f653213aed4f0975d990e25253
James Cook <mokus@deepbondi.net>
2010-09-13 18:58:48 +0000

    Bumped version (because 0.1 has been released) and fixed some stupid mistakes in Bisection.hs

--- src/Math/Root/Finder/Bisection.hs
+++ src/Math/Root/Finder/Bisection.hs
@@ -17,18 +17,19 @@ instance (Fractional a, Ord b, Num b) => RootFinder Bisect a b where
         | otherwise = Bisect x2 f2 (x1-x2)
         where f1 = f x1; f2 = f x2
     
-    stepRootFinder f orig@(Bisect x fx dx) = case fMid `compare` 0 of
+    stepRootFinder _ orig@(Bisect _ _ 0) = orig
+    stepRootFinder f orig@(Bisect x fx dx)
+        | x == xMid = orig
+        | otherwise = case fMid `compare` 0 of
             LT ->  Bisect xMid fMid dx2
-            EQ ->  orig
+            EQ ->  Bisect xMid fMid dx2
             GT ->  Bisect x fx dx2 
-            where
-                dx2 = dx * 0.5
-                xMid = x + dx2
-                fMid = f xMid
+        where
+            dx2 = dx * 0.5
+            xMid = x + dx2
+            fMid = f xMid
     
     estimateRoot  (Bisect x _  _) = x
-    
-    estimateError (Bisect _ 0  _) = 0
     estimateError (Bisect _ _ dx) = dx
 
 -- |Using bisection, return a root of a function known to lie between x1 and x2.
